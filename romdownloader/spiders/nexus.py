cf6a1f92f6831aca20c1dfe49757b93de819f4c4
Dongdongshe <shrek.s@hotmail.com>
2015-03-25 01:59:30 -0700

    add init file to fix a running error

--- romdownloader/spiders/nexus.py
+++ romdownloader/spiders/nexus.py
@@ -12,16 +12,8 @@ class RomSpider(CrawlSpider):
     allowed_domains = ["developer.google.com"]
     start_urls = [
          "https://developers.google.com/android/nexus/images"   
-#         "http://www.clockworkmod.com/rommanager/",   
-#        "http://www.clockworkmod.com/rommanager/device/a700/developer/google?manifest=http%3A%2F%2Fgh-pages.clockworkmod.com%2FROMManagerManifest%2Fgapps.js&name=Google%20Apps&deviceName=Acer%20A700",
         ]
     
-#    rules = (
-#        Rule(LinkExtractor(restrict_xpaths=('//tbody', )), callback='parse_url'),
-#        )
-        
-#    def parse_start_url(response):
-#        response.xpath('//')    
     def parse(self, response):
         urls = response.xpath('//table/tr')
         items = []
@@ -31,22 +23,3 @@ class RomSpider(CrawlSpider):
             item['url'] = url.xpath('td/a/@href').extract()
             items.append(item)
         return items
-#    def parse_url(self, response):
-#        urls = response.url
-#        items = []
-#        for url in urls:
-#            item = FileItem()
-#            item['file_url'] = url
-#            items.append(item)
-
-
-#        files = response.xpath('//tbody/tr')
-#        items = []
-#
-#        for file in files:
-#            item = FileItem()
-#            item['name'] = file.xpath('td/h4/text()').extract()
-#            item['file_url'] = file.xpath('td/a/@href').extract()
-#            items.append(item)
-#        return items
-
