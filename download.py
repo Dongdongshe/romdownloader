63459dccf55e024b22486f00feed18be0fcf0ae9
Dongdongshe <shrek.s@hotmail.com>
2015-03-24 23:33:56 -0700

    add download.py

---
+++ download.py
@@ -0,0 +1,32 @@
+import json
+import urllib2
+
+with open('roms.json') as data_file:    
+    data = json.load(data_file)
+
+for i in range(len(data)):
+    if not data[i]['url']:
+        continue
+        
+    url=data[i]['url'].pop().encode('ascii', errors='ignore')
+    file_name = url.split('/')[-1]
+    u = urllib2.urlopen(url)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8)*(len(status)+1)
+        print status,
+    
+    f.close()
+
