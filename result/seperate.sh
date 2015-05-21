cc3e4a4f140c402a7172aefd16ae578cb845e581
Dongdong <sherman@U12.(none)>
2015-05-21 16:22:26 -0700

    add out files

--- result/seperate.sh
+++ result/seperate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while read line 
 do
-echo $(cut -d ';' -f1 <<< "$line") >> miui.out.c 
-echo $(cut -d ';' -f2 <<< "$line") >> miui.out.c 
-echo $(cut -d ';' -f3 <<< "$line") >> miui.out.c 
-echo $(cut -d ';' -f4 <<< "$line") >> miui.out.c 
-echo $(cut -d ';' -f5 <<< "$line") >> miui.out.c 
+echo $(cut -d ';' -f1 <<< "$line") >> $2 
+echo $(cut -d ';' -f2 <<< "$line") >> $2
+echo $(cut -d ';' -f3 <<< "$line") >> $2
+echo $(cut -d ';' -f4 <<< "$line") >> $2
+echo $(cut -d ';' -f5 <<< "$line") >> $2 
 done < $1
